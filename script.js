e3c312b782280092832478c79a22473a2e536a20
Zaheer Dhalla <132003231+Zaheer-Dhalla@users.noreply.github.com>
2024-03-19 01:20:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -730,22 +730,20 @@ class Canvas {
 playMainTheme() {
     // This conditional is used to only play the main music ONCE as soon as the user interacts with the canvas
     if (this.musicPlaying === false) {
-        // This nested conditional loops the main music so it plays continuously
-        if (typeof this.mainMusic.loop === 'boolean') {
-            this.mainMusic.loop = true;
-            this.mainMusic.play();
-            this.musicPlaying = true;
-        } else {
-            // Preserve the correct reference to the class instance
-            const self = this;
-            this.mainMusic.addEventListener('ended', function () {
-                // Use the preserved reference to access the class instance
-                self.mainMusic.currentTime = 0;
-                self.mainMusic.play();
-            }, false);
-        }
+      // This nested conditional loops the main music so it plays continuously
+      if (typeof this.mainMusic.loop == 'boolean') {
+        this.mainMusic.loop = true;
+        this.mainMusic.play();
+        this.musicPlaying = true;
+      }
+      else {
+        this.mainMusic.addEventListener('ended', function() {
+          this.currentTime = 0;
+          this.play();
+        }, false);
+      }
     }
-}
+  }
 
   // ---------- ON CLICK ACTIONS
   // This method should be called whenever any button is clicked on the screen
